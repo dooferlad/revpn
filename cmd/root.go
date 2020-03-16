4c917a009688a8f90e15507f3971cc1015e4804a
Igor Gotlibovych <igor.gotlibovych@gmail.com>
2020-03-16 13:38:12 +0000

    Add config load error output

--- cmd/root.go
+++ cmd/root.go
@@ -84,5 +84,8 @@ func initConfig() {
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	} else {
+		fmt.Println("Error loading config ", viper.ConfigFileUsed(), ": ", err)
+		os.Exit(1)
 	}
 }
